ef4064c2cf023fd59cd3f514bc5e29e676e83a6b
silvio usai <berzylius@hotmail.it>
2021-04-12 15:35:13 +0200

    es1 fatto

---
+++ es1/script.js
@@ -0,0 +1,12 @@
+// Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età. 
+//Stampare a schermo attraverso il for in tutte le proprietà.
+
+var studente = {
+    "nome": "Silvio",
+    "cognome": "Usai",
+    "eta":"25",
+};
+
+for (var key in studente) {
+    console.log(studente[key]);
+    }
---
+++ es2/script.js
@@ -0,0 +1,3 @@
+// Creare un array di oggetti di studenti. 
+// Ciclare su tutti gli studenti e stampare per ognuno nome e cognome.
+
